88264f77153e9e1680687593d0b92b023d853ecc
jrapisarda <jrapisarda@live.com>
2025-10-17 05:46:16 -0600

    Removing AI Slop

--- src/agentforge/observability/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Observability utilities."""
-
-from .logging import configure_logging
-from .tracing import Span, Tracer
-
-__all__ = ["configure_logging", "Span", "Tracer"]
--- src/agentforge/persistence/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Persistence helpers."""
-
-from .database import init_db, record_agent_runs
-from .models import AgentRunRecord
-
-__all__ = ["init_db", "record_agent_runs", "AgentRunRecord"]
